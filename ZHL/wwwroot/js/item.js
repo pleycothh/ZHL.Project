6d086203bbb67f11647637eda292edae15ea47de
ben.li <ben.li@rightcrowd.com>
2022-09-28 14:37:11 +1000

    try to fix API error

--- ZHL/wwwroot/js/item.js
+++ ZHL/wwwroot/js/item.js
@@ -6,36 +6,25 @@ function deleteItem(sender, onFilter) {
     // Called like: openDialog(this), so we get a reference to the table row that called this function
     lastSender = sender;
 
-    console.log("Delete Item: " + sender + onFilter);
-
     let tr = lastSender;
 
-    console.log("tr:" + tr)
-    // server provides this for each row and hides it,
-    // here we use the provided hash to uniquely identify this row
     let hash = tr.querySelector('.hash').textContent;
 
-    console.log("hash:" + hash);
- //   let newNote = popup.querySelector('.notes').value;
- //
- //   const cacheName = "tempId";
- //   
- //   //const urlParams = new URLSearchParams(window.location.search);
- //   //const cacheName = urlParams.get('CacheName');
- //
- //   //tr.querySelector('.notes').textContent = newNote;
- //
      if (!onFilter) {
 
-         console.log("item is:");
-         fetch(`/api/deleteItem/${"tempId"}/${hash}/`);
+         console.log(hash + " - Item deleted!");
+         Promise.resolve(
+            fetch(`/api/deleteItem/${hash}/`)
+         );
      }
      if (onFilter) {
   
-         console.log("filter is:");
-         fetch(`/api/deleteFilter/${"tempId"}/${hash}/`);
+         console.log( hash + " - Filter deleted!");
+         Promise.resolve(
+             fetch(`/api/deleteFilter/${hash}/`)
+         );
      }
-    
+    tr.style.display = 'none';
 
     lastSender = undefined;
 }
