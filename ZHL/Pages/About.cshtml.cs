e957964101d144291d679e6b714a39b95f51a61b
ben.li <ben.li@rightcrowd.com>
2022-10-01 14:12:15 +1000

    update emailer setting

--- ZHL/Pages/About.cshtml.cs
+++ ZHL/Pages/About.cshtml.cs
@@ -3,6 +3,7 @@ using ZHL.Library.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ZHL.GUI.Provider.Contracts;
+using System.Text;
 
 namespace ZHL.GUI.Pages
 {
@@ -22,7 +23,7 @@ namespace ZHL.GUI.Pages
         [BindProperty]
         public string TextBox { get; set; }
         [BindProperty]
-        public string To { get; set; }
+        public string UserEmail { get; set; }
         [BindProperty]
         public string Subject { get; set; }
 
@@ -35,7 +36,18 @@ namespace ZHL.GUI.Pages
 
         public IActionResult OnPostSendEmail()
         {
-            _emailProvider.Sender(new EmailClientModel(Name, To, Subject, TextBox));
+            /// Send Email to me
+            _emailProvider.Sender(new EmailClientModel(Name, "ben.li19930110@gmail.com", Subject, TextBox));
+
+
+            /// Send conformation email to user
+            StringBuilder sb = new();
+            sb.AppendLine($"Hi {Name}, This is Ben Li");
+            sb.AppendLine($"Thank you for your {Subject}");
+            sb.AppendLine("Your Request is receved, and I will contact your shortly !");
+            sb.AppendLine("Email: <a>ben.li19930119@gmail.com</a>");
+
+            _emailProvider.Sender(new EmailClientModel(Name, UserEmail, "Thank You!", sb.ToString()));
 
             return RedirectToPage("./About", new { CacheId });
 
